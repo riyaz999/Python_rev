8f0eb578618f8bc13783c4c6a16589a91b3ee33a
riyaz999 <mohammed_riyaz999@yahoo.com>
2022-10-15 22:56:03 -0700

    def testing

---
+++ def.py
@@ -0,0 +1,18 @@
+x = 'hello bob'
+print(x)
+def hurry():
+    print('you need to move fast')
+    print('you gonna miss the bus')
+hurry()
+
+def greet(lang):
+    if lang == 'es':
+        print('english')
+    elif lang == 'sh':
+        print("bash")
+    else:
+        print('blast')
+
+greet('es')
+greet('sh')
+greet('kiki')
