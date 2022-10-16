653449bb198070121d2167fa203553cc1c3e1b7c
riyaz999 <mohammed_riyaz999@yahoo.com>
2022-10-15 23:36:23 -0700

    def

---
+++ def.py
@@ -0,0 +1,10 @@
+def greet(lang):
+    if lang == 'es':
+        return 'english'
+    elif lang == 'sh':
+        return "bash"
+    else:
+        return 'blast'
+
+print(greet('es'))
+print(greet("sh"))
