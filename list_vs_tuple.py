ae4699f41ee43121677622012f03ccb7963350fd
riyaz999 <mohammed_riyaz999@yahoo.com>
2022-10-23 00:03:33 -0700

    lists vs tuple

---
+++ list_vs_tuple.py
@@ -0,0 +1,14 @@
+# List are mutatable
+arctic = ['5', '6', '9']
+arctic[2] = '999'
+print(len(arctic))
+
+# Tuple are non- mutatable
+
+arctic_ice = ('5', '6', '9')
+#arctic_ice[2] = 20000
+print(arctic_ice)
+
+Nxt = ['abc']
+print(len('Nxt'))
+print(Nxt[0])
