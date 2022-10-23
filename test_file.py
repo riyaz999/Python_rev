efcfae3af5400452d3a85cf22e96e82a4f643b7b
riyaz999 <mohammed_riyaz999@yahoo.com>
2022-10-22 23:26:35 -0700

    removing unwanted print statements.

--- test_file.py
+++ test_file.py
@@ -6,12 +6,8 @@ kiki = dict()
 for w in hand:
     line = w.rstrip()
     line = line.split()
-    # print(line)
     for i in line:
         kiki[i] = kiki.get(i, 0) + 1
-        # print(i)
-# print(kiki)
-print(kiki.items())
 largest = -1
 theword = None
 for k, v in kiki.items():
